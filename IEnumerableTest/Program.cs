a231c63e8e26a4e39dd419f09ef96a901d36ec16
Dennis Renz <dennis@r-renz.de>
2020-09-08 21:42:37 +0200

    Boiled down to essence

--- IEnumerableTest/Program.cs
+++ IEnumerableTest/Program.cs
@@ -6,49 +6,28 @@ namespace IEnumerableTest
 {
     class Program
     {
-        static IEnumerable<int> MakeAndReturnIEnumerable(IEnumerable<int> values)
+        static IEnumerable<int> SelectWithoutToList(IEnumerable<int> values)
             => values.Select(v => { Console.WriteLine("Select with IEnumerable"); return v + 1; });
 
-        static IList<int> MakeAndReturnIList(IEnumerable<int> values)
+        static IList<int> SelectWithToList(IEnumerable<int> values)
             => values.Select(v => { Console.WriteLine("Select with IList"); return v + 1; }).ToList();
 
-        static IEnumerable<int> MakeListAndReturnIEnumerable(IEnumerable<int> values)
-            => values.Select(v => { Console.WriteLine("Select with IList"); return v + 1; }).ToList();
+        static void ForeachTwice(IEnumerable<int> projected, string caption)
+        {
+            Console.WriteLine(caption);
+            Console.WriteLine("------------------------------------------");
+            foreach(var p in projected)
+                Console.WriteLine($"Projected value is {p}");
+            foreach (var p in projected)
+                Console.WriteLine($"Projected value is {p}");
+            Console.WriteLine("");
+        }
 
         static void Main(string[] args)
         {
             var values = new List<int> { 0 };
-
-            // IEnumerable twice
-            Console.WriteLine("IEnumerable twice");
-            Console.WriteLine("------------------------------------------");
-            var projectedWithIEnumerable = MakeAndReturnIEnumerable(values);
-            foreach (var p in projectedWithIEnumerable)
-                Console.WriteLine($"Selected value is {p}");
-            foreach (var p in projectedWithIEnumerable)
-                Console.WriteLine($"Selected value is {p}");
-            Console.WriteLine("");
-
-            // IList twice
-            Console.WriteLine("IList twice");
-            Console.WriteLine("------------------------------------------");
-            var projectedWithIList = MakeAndReturnIList(values);
-            foreach (var p in projectedWithIList)
-                Console.WriteLine($"Selected value is {p}");
-            foreach (var p in projectedWithIList)
-                Console.WriteLine($"Selected value is {p}");
-            Console.WriteLine("");
-
-
-            // List -> IEnumerable twice
-            Console.WriteLine("List used via IEnumerable twice");
-            Console.WriteLine("------------------------------------------");
-            var projectedWithIListReturnIEnumerable = MakeListAndReturnIEnumerable(values);
-            foreach (var p in projectedWithIListReturnIEnumerable)
-                Console.WriteLine($"Selected value is {p}");
-            foreach (var p in projectedWithIListReturnIEnumerable)
-                Console.WriteLine($"Selected value is {p}");
-            Console.WriteLine("");
+            ForeachTwice(SelectWithoutToList(values), "Without .ToList()");
+            ForeachTwice(SelectWithToList(values), "With .ToList()");
         }
     }
 }
