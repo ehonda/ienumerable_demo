ed9fe83b4b7e3865677053885f4dcc1c6a409452
Dennis Renz <dennis@r-renz.de>
2020-09-08 21:30:31 +0200

    Initial commit

---
+++ IEnumerableTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerableTest
+{
+    class Program
+    {
+        static IEnumerable<int> MakeAndReturnIEnumerable(IEnumerable<int> values)
+            => values.Select(v => { Console.WriteLine("Select with IEnumerable"); return v + 1; });
+
+        static IList<int> MakeAndReturnIList(IEnumerable<int> values)
+            => values.Select(v => { Console.WriteLine("Select with IList"); return v + 1; }).ToList();
+
+        static IEnumerable<int> MakeListAndReturnIEnumerable(IEnumerable<int> values)
+            => values.Select(v => { Console.WriteLine("Select with IList"); return v + 1; }).ToList();
+
+        static void Main(string[] args)
+        {
+            var values = new List<int> { 0 };
+
+            // IEnumerable twice
+            var projectedWithIEnumerable = MakeAndReturnIEnumerable(values);
+            foreach (var p in projectedWithIEnumerable)
+                Console.WriteLine($"Selected value is {p}");
+            foreach (var p in projectedWithIEnumerable)
+                Console.WriteLine($"Selected value is {p}");
+
+            // IList twice
+            var projectedWithIList = MakeAndReturnIList(values);
+            foreach (var p in projectedWithIList)
+                Console.WriteLine($"Selected value is {p}");
+            foreach (var p in projectedWithIList)
+                Console.WriteLine($"Selected value is {p}");
+
+            // IList -> IEnumerable twice
+            var projectedWithIListReturnIEnumerable = MakeListAndReturnIEnumerable(values);
+            foreach (var p in projectedWithIListReturnIEnumerable)
+                Console.WriteLine($"Selected value is {p}");
+            foreach (var p in projectedWithIListReturnIEnumerable)
+                Console.WriteLine($"Selected value is {p}");
+
+        }
+    }
+}
